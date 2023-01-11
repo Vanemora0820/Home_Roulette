072a6aff6ca6889db47bc1a986b0ce8b6dc42618
darius1751 <lupequi12@gmail.com>
2023-01-11 13:05:26 -0500

    Colocando JtextsPlayers estaticos, y arreglando componentes en el panel

--- src/main/java/org/homeRoulette/Views/Home.java
+++ src/main/java/org/homeRoulette/Views/Home.java
@@ -1,19 +1,24 @@
 package org.homeRoulette.Views;
 
 import javax.swing.*;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.util.ArrayList;
 
 public class Home extends JFrame {
-    public Home (){
-        this.panel =new JPanel();
-        this.title = new JLabel();
-        initialConfiguration();
-    }
     private JPanel panel;
     private JLabel title;
+    private ArrayList<JTextField> players;
+    public Home (){
+        this.panel = new JPanel();
+        this.title = new JLabel();
+        this.players = new ArrayList<>();
+        initialConfiguration();
+    }
+
     private void initialConfiguration(){
         this.setVisible(true);
-        this.setSize(500,500);
+        this.setSize(650,500);
         this.setLayout(null);
         this.setLocationRelativeTo(null);
         this.setResizable(false);
@@ -21,22 +26,40 @@ public class Home extends JFrame {
         this.setTitle("Game");
         configurationPanel();
         configurationTitle();
+        configurationJTextsPlayers();
         useComponents();
     }
 
     private void configurationPanel(){
         this.panel.setBackground(Color.decode("#C5EFF9"));
-        this.panel.setSize(500,500);
+        this.panel.setLayout(null);
+        this.panel.setSize(650,500);
+
     }
     private void configurationTitle(){
         this.title.setText("Home_Roulette");
-        this.title.setForeground(Color.BLACK    );
+        this.title.setForeground(Color.BLACK);
         Font font = this.title.getFont().deriveFont(Font.ITALIC,40);
         this.title.setFont(font);
+        this.title.setSize(500,50);
+        this.title.setLocation(200,20);
+    }
+    private void configurationJTextsPlayers(){
+        JTextField player1 = new JTextField();
+        player1.setToolTipText("Ingrese su nombre");
+        player1.setBounds(50,100,200,30);
+        JTextField player2 = new JTextField();
+        player2.setToolTipText("Ingrese su nombre");
+        player2.setBounds(350,100,200,30);
+        this.players.add(player1);
+        this.players.add(player2);
     }
 
     private  void useComponents(){
         this.add(panel);
         this.panel.add(title);
+        for(JTextField player : players){
+            this.panel.add(player);
+        }
     }
 }
