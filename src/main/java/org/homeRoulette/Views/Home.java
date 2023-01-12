135a88d82c6c4b35fac3c56f1c392f855cd052bf
darius1751 <lupequi12@gmail.com>
2023-01-11 19:36:03 -0500

    Agregando labels de los 2 jugadores

--- src/main/java/org/homeRoulette/Views/Home.java
+++ src/main/java/org/homeRoulette/Views/Home.java
@@ -1,39 +1,47 @@
 package org.homeRoulette.Views;
 
 import javax.swing.*;
-import java.awt.Color;
-import java.awt.Font;
+import java.awt.*;
 import java.util.ArrayList;
 
 public class Home extends JFrame {
+
+    private final int WIDTH;
+    private final int HEIGHT;
     private JPanel panel;
     private JLabel title;
     private ArrayList<JTextField> players;
+    private ArrayList<JLabel> labelsPlayers;
     public Home (){
         this.panel = new JPanel();
         this.title = new JLabel();
         this.players = new ArrayList<>();
+        this.labelsPlayers = new ArrayList<>();
+        this.WIDTH = 650;
+        this.HEIGHT = 500;
         initialConfiguration();
     }
 
     private void initialConfiguration(){
         this.setVisible(true);
-        this.setSize(650,500);
+        this.setSize(WIDTH,HEIGHT);
         this.setLayout(null);
         this.setLocationRelativeTo(null);
-        this.setResizable(false);
+        //this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Game");
         configurationPanel();
         configurationTitle();
+        configurationJLabelsPlayers();
         configurationJTextsPlayers();
         useComponents();
+        this.repaint();
     }
 
     private void configurationPanel(){
         this.panel.setBackground(Color.decode("#C5EFF9"));
         this.panel.setLayout(null);
-        this.panel.setSize(650,500);
+        this.panel.setSize(WIDTH,HEIGHT);
 
     }
     private void configurationTitle(){
@@ -43,6 +51,16 @@ public class Home extends JFrame {
         this.title.setFont(font);
         this.title.setSize(500,50);
         this.title.setLocation(200,20);
+    }
+    private void configurationJLabelsPlayers(){
+        JLabel labelPlayer1 = new JLabel("Player1");
+        JLabel labelPlayer2 = new JLabel("Player2");
+        labelPlayer1.setLocation(50,70);
+        labelPlayer2.setLocation(350,70);
+        labelPlayer1.setSize(200,30);
+        labelPlayer2.setSize(200,30);
+        this.labelsPlayers.add(labelPlayer1);
+        this.labelsPlayers.add(labelPlayer2);
     }
     private void configurationJTextsPlayers(){
         JTextField player1 = new JTextField();
@@ -58,8 +76,11 @@ public class Home extends JFrame {
     private  void useComponents(){
         this.add(panel);
         this.panel.add(title);
+        int i = 0;
         for(JTextField player : players){
+            this.panel.add(this.labelsPlayers.get(i));
             this.panel.add(player);
+            i++;
         }
     }
 }
